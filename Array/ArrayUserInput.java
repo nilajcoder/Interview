9ff56234857de2571129065dc6f43f649d53bd7f
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-01-17 16:21:08 +0000

    arrayuserinput

---
+++ Array/ArrayUserInput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class ArrayUserInput {
+
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the size of the array = ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];//array declaration
+
+
+        System.out.print("Enter the Element of the array = ");
+
+        //input
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.print("Array Elements Are =");
+
+        //display
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
