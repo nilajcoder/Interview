8b24508f1a901a6062e4b7d6b9bc8198f2bf2270
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-01-18 14:31:09 +0000

    Pallindrome

--- Java/Pallindrome.java
+++ Java/Pallindrome.java
@@ -5,22 +5,22 @@ public class Pallindrome {
     public static void main(String[] args) {
         
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a Number = ");
+        System.out.print("Enter a Number = ");//54
         int a=sc.nextInt();
         int n;
         int s=0;
-         n=a;
-        while (a>0) {
-        int r=a%10;
-        s=s*10+r;
-        a=a/10;   
+         n=a;//54
+        while (a>0) { //54>0  5>0
+        int r=a%10; //54%10=4  5%10=5
+        s=s*10+r;//0*10+4=4    0*10+5=5   45
+        a=a/10;  //54/10=5   5/10=0
         }
-        System.out.println("Reverse Number is = "+s);
-       if (s==n) {
+        System.out.println("Reverse Number is = "+s); //45
+       if (s==n) { //45==54
         System.out.println("It is a Pallindromde");
        }
          else{
-            System.out.println(" Not Pallindrome ");
+            System.out.println(" Not Pallindrome "); //not pallin
         
         
        }
