1932a92797da22606c8be97b5351d1b4e66fedb9
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-02-10 17:15:47 +0000

    Static and Non static Methods

---
+++ Java/Methods.java
@@ -0,0 +1,24 @@
+
+public class Methods {
+
+    public static void main(String[] args) {
+        Methods s=new Methods();
+        System.out.println("hii");
+        s.hello();
+        /*
+         * hello();
+         */
+        
+    }
+
+    public  void hello(){
+        System.out.println("Hello");
+    }
+
+    /*
+    public static void hello(){
+        System.out.println("Hello");
+    }
+     * 
+     */
+}
