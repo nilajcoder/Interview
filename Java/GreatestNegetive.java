4a1a906e6e537fc77a7b55df6e0351ea44d5cbfc
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-02-13 15:44:43 +0000

    GreatestNegativeValue

--- Java/GreatestNegetive.java
+++ Java/GreatestNegetive.java
@@ -5,13 +5,17 @@ public class GreatestNegetive {
     public static void main(String[] args) {
         
        Scanner sc=new Scanner(System.in);
-       System.out.println("Enter a Number");
-       int a=sc.nextInt();
-       if(a==-1){
-        System.out.println("It is a greatest Negative Number");
-       }else{
-        System.out.println("It is not greatest Negative number");
+       int [] arr={1,2,3,4,-2,-3,-5};
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<arr.length;i++){
+        if(arr[i]<0){
+            if (arr[i]>max) {
+                max=arr[i];
+                
+            }
+        }
        }
+       System.out.println("Greatest Negative value "+max);
 
 
     }
