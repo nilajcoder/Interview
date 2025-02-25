7be5d83cdae30180e3dde0b90135d13375708e2d
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-02-25 19:44:53 +0000

    HelloWorld

---
+++ Java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/*
+ * Write a program to print your name.
+
+ */
+
+ public class HelloWorld {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+    }
+ }
