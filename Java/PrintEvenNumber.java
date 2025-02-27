8729c15b0debc67103ad94711252ab0efdf94dd8
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-02-27 18:35:35 +0000

    PrintEvenOddandSum

--- Java/PrintEvenNumber.java
+++ Java/PrintEvenNumber.java
@@ -1,6 +1,8 @@
 /*
  * WAP Print Even Numbers Till  n
  * and Print Odd Numbers
+ * and Print the sum of Odd numbers and Even Numbers 
+ * 
  */
 
 import java.util.Scanner;
@@ -11,21 +13,28 @@ public class PrintEvenNumber {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number of Element");
         int n=sc.nextInt();
+        int evensum=0;
+        int oddsum=0;
         System.out.println("Even Numbers Are ");
         for(int i=1;i<=n;i++){
             if (i%2==0) {
                 System.out.println(i);
+                evensum+=i;
                 
             } 
         }
+        System.out.println("Sum of Even Numbers ="+evensum);
+
 
         System.out.println("Odd Numbers Are ");
         for(int i=1;i<=n;i++){
             if (i%2!=0) {
                 System.out.println(i);
+                oddsum+=i;
                 
             }
         }
+        System.out.println("Sum of Odd Numbers = "+oddsum);
 
     }
  }
