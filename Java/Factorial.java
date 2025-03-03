586ce42316d58cdd3e16f6c1220fc51d867eed14
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-03-03 14:35:18 +0000

    Factorial

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -40,4 +40,24 @@ public class Factorial {
 
     
 
-    
+    /*
+     * 
+     * dry run
+     * -------
+     * System.out.println("Enter a number"); 5
+        int fact=1;
+        int a=sc.nextInt(); 5
+        for(int i=1;i<=a;i++){   1<=5   i++ 2<=5 i++  3<=5 i++ 4<=5 i++  5<=5  6<=5 false so terminate the loop
+            fact=fact*i;        fact=1*1=1
+                                fact=1*2=2
+                                fact=2*3=6
+                                fact=6*4=24
+                                fact=24*5=  120
+
+
+           
+
+        }
+
+        System.out.println("Factorial of a number = "+fact); 120
+     */
