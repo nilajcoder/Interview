a1f3f20d757353ea700b7adafb02c77b54c95365
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-03-03 19:37:44 +0000

    EvenOddcountsumarray

--- Array/EvenOdd.java
+++ Array/EvenOdd.java
@@ -3,29 +3,40 @@
  * 
  * Count Even and Odd Numbers
 Write a program to count the number of even and odd numbers in an array of integers.
+and print the sum of even numbers and odd numbers 
+and print the even numbers and odd numbers 
  */
 public class EvenOdd {
-
-
     public static void main(String[] args) {
-        int arr []={1,2,3,4,5,6,7,8,9,10};
-        int evencount=0,oddcount=0;
-        for(int i=0;i<arr.length;i++){
-            if (arr[i]%2==0) {
-                evencount++;
-                System.out.println("Even numbers are "+arr[i]+ " ");
-            } else{
-                oddcount++;
-                System.out.println(arr[i]+" ");
-            }
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int evencount = 0, oddcount = 0, evensum = 0, oddsum = 0;
 
+        System.out.print("Even numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evencount++;
+                evensum += arr[i];
+                System.out.print(arr[i] + " ");  // Print even numbers
+            }
         }
 
-        System.out.println("Even numbers are = "+evencount);
-        System.out.println("Odd numbers are = "+oddcount);
+        System.out.print("\nOdd numbers: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                oddcount++;
+                oddsum += arr[i];
+                System.out.print(arr[i] + " ");  // Print odd numbers
+            }
+        }
+
+        System.out.println("\nEven numbers count = " + evencount);
+        System.out.println("Odd numbers count = " + oddcount);
+        System.out.println("Sum of Even numbers = " + evensum);
+        System.out.println("Sum of Odd numbers = " + oddsum);
     }
-    
 }
+
+
 
 
 /*
