6fe95fd0b446025251365da8d2b6d4fc9972752d
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-03-04 07:34:17 +0000

    FactorsPerfectSum

--- Java/Factors.java
+++ Java/Factors.java
@@ -3,6 +3,10 @@
 /*
  * WAP Find a factors of an integer
  * and print the sum of factors 
+ * 
+ * Check the the number Perfect number or not?
+ * If input is 6 =1+2+3=6 perfect
+ * 28= 1 +2+4+7+14=28 perfect
  */
 
 import java.util.Scanner;
@@ -16,15 +20,37 @@ public class Factors {
     int s=0;
     int n =sc.nextInt();
     System.out.println("Factors of  "+n+" are =");
+     
 
+    /*
+    Print the factors of numbers including number
+    Logic
+    */
     for(int i=1;i<=n;i++){
         if (n%i==0) {
             System.out.println(i);
+            
+        }
+    }
+   
+
+    /*
+
+    sum of factors exculding the number
+    Logic of Perfect number
+    */
+    for(int i=1;i<n;i++){
+        if (n%i==0) {
             s=s+i;
             
         }
     }
-       System.out.println("Sum Of the factors = "+s);
+    if(s==n){
+        System.out.println("The numbers "+n +" is Perfect number");
+    }else{
+        System.out.println("The number "+n+" is not Perfect number ");
+    }
+       System.out.println("Sum Of the factors excluding the number is "+n +" = "+s);
    }          
 }
 
@@ -57,5 +83,18 @@ i = 3	              5 % 3 != 0 (❌)	No	s = 1	-
 i = 4	               5 % 4 != 0 (❌)	No	s = 1	-
 i = 5	               5 % 5 == 0 (✅)	Yes         s = 1 + 5 = 6	 5
 
+
+
+
+
+Enter a number 
+6
+Factors of  6 are =
+1
+2
+3
+6
+The numbers 6 is Perfect number
+Sum Of the factors excluding the number is 6 = 6
  * 
  */
