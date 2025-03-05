f50b955c345f28cdaf9cca47406b7164fcd4d740
nilajhub <nilaj.chakraborty007@gmail.com>
2025-03-05 15:51:33 +0530

    Lamda example

---
+++ Java 8/Myinter.java
@@ -0,0 +1,9 @@
+package java8.practice;
+
+
+@FunctionalInterface
+
+public interface Myinter {
+	public abstract void sayhello();
+
+}
---
+++ Java 8/SayhelloLambda.java
@@ -0,0 +1,16 @@
+package java8.practice;
+
+public class SayhelloLambda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Myinter i=()->{
+			System.out.println("hii hello Lamda");
+			
+		};
+		
+		i.sayhello();
+
+	}
+
+}
