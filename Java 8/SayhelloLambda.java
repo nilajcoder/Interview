fc0b0a91de4b76af818f9102621ef6457b45b6e3
nilajhub <nilaj.chakraborty007@gmail.com>
2025-03-05 15:52:51 +0530

    Lamda example

--- Java 8/SayhelloLambda.java
+++ Java 8/SayhelloLambda.java
@@ -1,3 +1,24 @@
+
+/*
+ * 
+ * /*
+ * What is lambda Expression?
+ * Lambda expression is an anonymous function
+ * Not having any name
+ * Not having any return type
+ * not having any modifier
+ * 
+ * Benifits
+ * --------
+ * More readable Concise
+ * To Enable Functional Programmimg
+ *   
+ * 
+ */
+
+
+
+
 package java8.practice;
 
 public class SayhelloLambda {
