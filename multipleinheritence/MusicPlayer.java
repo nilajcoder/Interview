e4dcb6c7591e40f4d5ac1ab273e3002a60c59adb
Nilaj Chakraborty <91656802+nilajcoder@users.noreply.github.com>
2025-03-10 17:30:46 +0000

    Interface

--- multipleinheritence/MusicPlayer.java
+++ multipleinheritence/MusicPlayer.java
@@ -1,3 +1,20 @@
+
+
+/*
+ * INTERFACE: 100% pure abstract class is called INTERFACE.
+            A blueprint for a class that Specifies a 
+            set of abstruct method that implementing  classes must define
+
+
+	An Interface contains behaviours that a class Implementation.
+
+interface is a class related  keyword.
+
+
+Note: Every method present inside interface is always PUBLIC and ABSTRACT whether declaring or not .
+ */
+
+
 package multipleinheritence.com;
 
 public interface MusicPlayer {
