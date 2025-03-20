927fa6bbfd9d5735a843a173e1de3af2e908e864
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-03-20 23:13:38 +0530

    Secondlargestnumber

--- Methods/SecondLargestNumber.java
+++ Methods/SecondLargestNumber.java
@@ -1,3 +1,29 @@
+
+
+/*
+ * 
+ * Find the second Largest number
+ * 
+ * 
+ * 	public static int seclargest(int arr[]) {
+		int largest = -1;
+		int seclarge = -1;
+		for (int num : arr) { 
+			if (num > largest) { 50>-1  56>50  98>56  55>98 false
+				seclarge = largest; -1  50  56
+				largest = num;      50   56  98
+			} else if (num > seclarge && num < largest) {    55>56 && 55<98 false
+				seclarge = num;
+			}
+
+		}
+		return seclarge; 56
+
+	}
+	
+ */
+
+
 package javainterviewquestiones.array;
 
 public class SecondLargestNumber {
@@ -21,7 +47,7 @@ public class SecondLargestNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[]= {50,56,98,55};
-		System.out.println("Second largest number is "+ seclargest(a));
+		System.out.println("Second largest number is = "+ seclargest(a));
 
 	}
 
